f4a4b7d7979a59d8d82ab55b235f350dd3a0307f
Ubuntu <ubuntu@ip-172-31-80-8.ec2.internal>
2019-10-16 11:06:51 +0000

    Added environment variables and rearranged file structure

---
+++ .bashrc
@@ -0,0 +1,1 @@
+export mongo_uri="mongodb+srv://Neil:BrooklynWooD@myfirstcluster-lxpo1.mongodb.net/milestone_3?retryWrites=true&w=majority"
--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'milestoneproject3me'
+app.config["MONGO_URI"] = os.environ.get('mongo_uri')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return "Hello World....again"
+@app.route('/get_activities')
+def get_activities():
+    return render_template("activities.html", activities=mongo.db.Activities.find())
     
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
