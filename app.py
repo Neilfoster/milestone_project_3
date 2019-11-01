c36a4c7daa9b782b38267dd2798b0023f674f827
Ubuntu <ubuntu@ip-172-31-80-8.ec2.internal>
2019-11-01 12:23:40 +0000

    updated filter dearch

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
@@ -16,12 +16,20 @@ def get_activities():
     return render_template("home.html", 
     activities=mongo.db.Activities.find())
     
-@app.route('/activity_view')
+@app.route('/activity_view', methods=['POST', 'GET'])
 def activity_view():
     return render_template('activity_view.html',
-    activities=mongo.db.Activities.find().limit(6), 
-    ages=mongo.db.ages.find(),
-    durations=mongo.db.duration.find())
+        #activities=mongo.db.Activities.find().limit(6), 
+        ages=mongo.db.ages.find(),
+        durations=mongo.db.duration.find(),
+        user_supplied_age_group = request.GET['age_group'],
+        user_supplied_activity_duration = request.GET['activity_duration'],
+        activities = mongo.db.Activities.find(
+               {'age_group': user_supplied_age_group,
+                 'activity_duration': user_supplied_activity_duration,
+               }))
+   
+    
     
 @app.route('/add_activity', methods=['POST', 'GET'])
 def add_activity():
@@ -40,3 +48,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
+        
